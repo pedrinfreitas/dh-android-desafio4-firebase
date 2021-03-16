3894e75a783b619d33a44eeeea51002b642c4e0f
Pedro Freitas <pedrinfreitas@gmail.com>
2021-03-16 20:16:02 -0300

    initial commit

---
+++ app/src/main/java/br/com/eupedro/desafio_firebase/MainActivity.kt
@@ -0,0 +1,11 @@
+package br.com.eupedro.desafio_firebase
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
